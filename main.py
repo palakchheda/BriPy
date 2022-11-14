3173d4a070a54a7ecb1bea3ed118b98e9f9dd608
palakchheda <42711310+palakchheda@users.noreply.github.com>
2022-11-13 20:48:50 -0800

    adding plusOne func

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+# add a one to large integer noted in a list
+'''
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+'''
 
-# Press the green button in the gutter to run the script.
+def plusOne(digits):
+    num = 0
+    for i in range(len(digits)):
+        num += digits[i] * pow(10, (len(digits)-1-i))
+        print(i)
+    return [int(i) for i in str(num+1)]
+
 if __name__ == '__main__':
-    print_hi('Palak')
+    digits = [1, 3, 9]
+    print(plusOne(digits))
+
 
