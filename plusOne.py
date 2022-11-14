3a22bdd24882d2b454c0cd1e43efa59279967e44
palakchheda <42711310+palakchheda@users.noreply.github.com>
2022-11-13 21:05:21 -0800

    adding plusOne func to a new file

---
+++ plusOne.py
@@ -0,0 +1,29 @@
+# This is a sample Python script
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# add a one to large integer noted in a list
+'''
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+'''
+
+def plusOne(digits):
+    num = 0
+    for i in range(len(digits)):
+        num += digits[i] * pow(10, (len(digits)-1-i))
+        print(i)
+    return [int(i) for i in str(num+1)]
+
+if __name__ == '__main__':
+    digits = [1, 3, 9]
+    print(plusOne(digits))
+
+
